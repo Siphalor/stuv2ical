71b949a19e02d2a946c5b06e8429cdeb23861512
Siphalor <info@siphalor.de>
2022-02-07 18:06:17 +0100

    Add message to explain missing lecturers

--- src/icalendar.rs
+++ src/icalendar.rs
@@ -33,7 +33,11 @@ async fn write_lecture<W: AsyncWrite + std::marker::Unpin>(writer: &mut W, lectu
     write_short_line(writer, format!("DTEND:{}", lecture.end_time().format(ICALENDAR_DATE_TIME_FORMAT)).as_str()).await?;
     write_field(writer, "SUMMARY", lecture.name()).await?;
     write_field(writer, "LOCATION", &lecture.rooms().join(", ")).await?;
-    write_field(writer, "DESCRIPTION", format!("Dozent:innen: {}", lecture.lecturers().join(" & "))).await?;
+    write_field(writer, "DESCRIPTION", if lecture.lecturers().is_empty() {
+        "Dozent:innen sind aufgrund von Datenschutzbedenken der DHBW nicht mehr öffentlich!".to_string()
+    } else {
+        format!("Dozent:innen: {}", lecture.lecturers().join(" & "))
+    }).await?;
 
     for lecturer in lecture.lecturers() {
         write_line(writer, format!("ATTENDEE;CN=\"{}\":noreply@mosbach.dhbw.de", lecturer).as_str()).await?;
